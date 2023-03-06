7d362227af002f4105fa9389da0044c0c1d75aab
Maxim Butuzov <101450815+mazzy8@users.noreply.github.com>
2023-03-06 19:38:17 +0500

    Update Resize_and_reduce_img.py
    optimization. add logging

--- Resize_and_reduce_img.py
+++ Resize_and_reduce_img.py
@@ -2,36 +2,46 @@
 from PIL import Image
 import os
 from datetime import datetime
+import logging
 
+
+logging.basicConfig(level=logging.INFO, filename=f"pdf_log_{datetime.now().date()}.log",
+                    format="%(asctime)s %(levelname)s %(message)s")
 
 FOLDER_AND_FILES = os.walk('.')
 FILES_COUNT = 0
 RESIZE_COUNT = 0
 DIF_SIZE = 0
 
-print(f'Start: {datetime.now()}')
+logging.info(f'#####################Start script#####################')
 for root, dirs, files in FOLDER_AND_FILES:
     for file in files:
-        FILES_COUNT += 1
         filepath = os.path.join(root, file)
-        if file[-3:].lower() in ('peg', 'jpg', 'img'):
+        if file[-3:].lower() in ('peg', 'jpg', 'img', 'png'):
+            FILES_COUNT += 1
             try:
                 size_file = os.path.getsize(filepath)
                 with Image.open(filepath) as im:
-                    print(f'{datetime.now()} ||| open: {filepath} ||| ', end=' ')
                     width, height = im.size
-                    if width > 1920:  # height > 1080
-                        height_percent = (1920 / width)  # width_percent = (1080 / height)
-                        new_height = int(height * height_percent)  # new_width = int(width * width_percent)
-                        im.thumbnail((1920, new_height), Image.LANCZOS)
-                        im.save(filepath, quality=85, optimize=True)
+                    if width > 1920 or height > 1080:
+                        while width > 1920 or height > 1080:
+                            if width > 1920:
+                                height_percent = (1920 / width)
+                                height = int(height * height_percent)
+                                width = 1920
+                            else:
+                                width_percent = (1080 / height)
+                                width = int(width * width_percent)
+                                height = 1080
+                        im.thumbnail((width, height), Image.LANCZOS)
+                        im.save(filepath, quality=80, optimize=True)
                         size_file_after = os.path.getsize(filepath)
                         dif_size = int((size_file - size_file_after)/1024)
                         DIF_SIZE += dif_size
                         RESIZE_COUNT += 1
-                        print(f'new size = {size_file_after} ||| difference: {dif_size}Kb')
+                        logging.info(f'Write file "{filepath}". Resize: {dif_size} Kb')
                     else:
-                        print(f'close, width <= 1920')
-            except OSError:
-                print('Error OS')
-print(f'Stop: {datetime.now()}, all_files = {FILES_COUNT}, resize_files = {RESIZE_COUNT}, diff_size = {DIF_SIZE}Kb')
+                        continue
+            except Exception as e:
+                logging.error(f'Error: {e}')
+logging.info(f'Stop script. All_image_files = {FILES_COUNT}, resize_files = {RESIZE_COUNT}, diff_size = {DIF_SIZE} Kb')
